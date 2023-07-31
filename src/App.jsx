2c94e80d3c87750116a69a2cf85a5fa065e8682b
Maxim <Sen989@yandex.ru>
2023-07-31 18:57:38 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import UploadToYandexDisk from './components/UploadToYandexDisk/UploadToYandexDisk';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [idToken, setIdToken] = useState("");
+  function onload() {
+    window.YaSendSuggestToken("https://maximzhe.github.io/Loader-Yandex-disk", {
+    kek: true,
+    });
+    }
+  useEffect(() => {
+    function load() {
+      window.YaAuthSuggest.init(
+        {
+          client_id: `${import.meta.env.VITE_API_KEY}`,
+          response_type: "token",
+          redirect_uri: "https://maximzhe.github.io/Loader-Yandex-disk",
+        },
+        "https://maximzhe.github.io/Loader-Yandex-disk",
+        {
+          view: "button",
+          parentId: "container",
+          buttonView: "main",
+          buttonTheme: "light",
+          buttonSize: "m",
+          buttonBorderRadius: 0,
+        }
+      )
+        .then(({ handler }) => handler())
+        .then((data) => setIdToken(data.access_token),onload())
+        .catch((error) => console.log("Обработка ошибки", error));
+    }
+    load();
+  }, [idToken]);
+  return (
+    
+    <>
+      <div>
+        
+      <UploadToYandexDisk idToken={idToken}/>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/UploadToYandexDisk/UploadToYandexDisk.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import axios from "axios";
+
+const FileUploader = ({ idToken }) => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [successfully, setSuccessfully] = useState(false);
+  const [isloading, setIsloading] = useState(false);
+  
+  const handleFileUpload = async () => {
+    const url = "https://cloud-api.yandex.net/v1/disk/resources/upload";
+
+    selectedFiles.forEach(async (file) => {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        setIsloading(true);
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: idToken,
+          },
+          params: {
+            path: `/${file.name}`,
+            overwrite: true,
+          },
+        });
+
+        const uploadUrl = response.data.href;
+        await axios.put(uploadUrl, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        setSuccessfully(true);
+        console.log(`File ${file.name} uploaded successfully.`);
+      } catch (error) {
+        setIsloading(false);
+        console.error(`Error uploading file ${file.name}:`, error);
+      }
+    });
+
+    setSelectedFiles([]);
+  };
+
+  const handleFileSelection = (event) => {
+    const files = Array.from(event.target.files);
+    if (files.length > 100) {
+      alert("You can't select more than 100 files.");
+      return;
+    }
+    setSelectedFiles(files);
+  };
+
+  return (
+    <div>
+      {idToken !== "" ? (
+        <div>
+          {successfully === false ? (
+            <div>
+              <div>
+                <input type="file" multiple onChange={handleFileSelection} />
+                <button onClick={handleFileUpload}>Upload</button>
+              </div>
+              {isloading === true ? <p>loading</p> : null}
+            </div>
+          ) : (
+            <p>Успешно отправлено!</p>
+          )}
+        </div>
+      ) : (
+        <div>
+          <h2>Login using Yandex</h2>
+          <div id="container"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
